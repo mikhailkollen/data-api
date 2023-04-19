ab598a4a07a30a783d1c9d98bf59d9168d25f3b2
MishaKollen <mikhailkollen@gmail.com>
2023-04-19 15:24:11 +0400

    try a different origin

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const connectDB = async () => {
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://mikhailkollen.github.io", "http://127.0.0.1:5500/"],
+    origin: "http://127.0.0.1:8000/",
   })
 );
 
