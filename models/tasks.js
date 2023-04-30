7cabe1708b1d36ae7c50af8f600d0b726c07e83e
MishaKollen <mikhailkollen@gmail.com>
2023-04-30 17:27:51 +0400

    add timestamps

--- models/tasks.js
+++ models/tasks.js
@@ -1,23 +1,26 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
-const TaskSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const TaskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    isCompleted: {
+      type: Boolean,
+      default: false,
+    },
+    tag: {
+      type: String,
+      default: "other",
+    },
+    date: {
+      type: String,
+      default: "2023-04-16T00:00:00.000Z",
+    },
   },
-  isCompleted: {
-    type: Boolean,
-    default: false,
-  },
-  tag: {
-    type: String,
-    default: "other",
-  },
-  date: {
-    type: String,
-    default: "2023-04-16T00:00:00.000Z",
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Task", TaskSchema);
